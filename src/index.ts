34fd33f13b1985a03859d28f14dca241b7b70f36
Dominic Bartl <dominic.bartl.at@gmail.com>
2017-06-15 16:09:33 +0200

    Export server

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,5 @@ export { BaseRepository } from './base_repository';
 export { BaseTestCase } from './base_test_case';
 export { Pagination } from './pagination';
 export { WebError } from './error';
+export { Server } from './server';
 export * from './router';
