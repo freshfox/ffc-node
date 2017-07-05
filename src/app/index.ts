8b497651c030b8fa5a70f297121edd01f8842b7e
Dominic Bartl <dominic.bartl.at@gmail.com>
2017-07-05 23:32:47 +0200

    Export all classes

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,20 @@
 import "reflect-metadata";
 
-export { BaseRepository } from './storage/base_repository';
-export { TestCase } from './test_case';
-export { BaseController } from './http/base_controller';
-export { Pagination } from './http/pagination';
-export { WebError } from './http/error';
-export { Server } from './http/server';
-export { Route, Router } from './http/router';
+// HTTP
+export {Authenticator} from './http/authenticator';
+export {BaseController} from './http/base_controller';
+export {WebError} from './http/error';
+export {Pagination} from './http/pagination';
+export {Route, Router} from './http/router';
+export {Server} from './http/server';
+export {Sorting} from './http/sorting';
+export {Validator} from './http/validator';
+
+// Storage
+export {BaseRepository} from './storage/base_repository';
+export {entity, property} from './storage/decorators';
+export {StorageDriver} from './storage/storage_driver';
+
+export {TYPES} from './types';
+
+export {TestCase} from './test_case';
