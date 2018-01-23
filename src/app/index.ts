65163c9bea92e908aca2c05d478f4f6708626ba7
Dominic Bartl <dominic.bartl.at@gmail.com>
2018-01-23 12:44:24 +0100

    Export ServiceProvider

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
+
 export * from './core/authenticator'
 export * from './core/storage_driver'
 export * from './core/types'
 export * from './core/kernel'
+export * from './core/service_provider'
 
 export * from './http/base_controller'
 export * from './http/jwt_authenticator'
