216175fad5a5279ceb89632bf27db162491274a1
Dominic Bartl <dominic.bartl.at@gmail.com>
2018-01-23 11:40:23 +0100

    Add BaseServiceProvider

---
+++ src/app/core/service_provider.ts
@@ -0,0 +1,27 @@
+import {Kernel} from './kernel';
+import {Container, interfaces} from 'inversify';
+
+export abstract class BaseServiceProvider {
+
+	private kernel = new Kernel();
+
+	constructor(overrides?: Kernel) {
+		this.registerDependencies();
+		if (overrides) {
+			overrides.setParent(this.kernel);
+			this.kernel = overrides;
+		}
+
+	}
+
+	protected abstract registerDependencies();
+
+	resolve<T>(constructorFunction: interfaces.Newable<T>): T {
+		return this.kernel.resolve(constructorFunction);
+	}
+
+	getContainer(): Container {
+		return this.kernel.getContainer();
+	}
+
+}
