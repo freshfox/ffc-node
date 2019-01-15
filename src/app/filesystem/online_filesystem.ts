bf587dba6d9e4a0b0d3afe7a032c2db8a5f6128f
Dominic Bartl <dominic.bartl.at@gmail.com>
2019-01-15 17:48:15 +0100

    Add validUntil to getUploadUrl()

--- src/app/filesystem/online_filesystem.ts
+++ src/app/filesystem/online_filesystem.ts
@@ -2,7 +2,7 @@ import {IFilesystem} from './filesystem';
 
 export interface IOnlineFilesystem extends IFilesystem {
 
-	getUploadUrl(path: string): Promise<string>;
+	getUploadUrl(path: string, validUntil: Date): Promise<string>;
 
 	getDownloadUrl(path: string, validUntil: Date): Promise<string>;
 
