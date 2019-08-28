e512659c692e9d52fe685e6149aa7a36373e2a0e
Dominic Bartl <dominic.bartl.at@gmail.com>
2019-08-28 11:24:32 +0200

    Add GetUrlOptions interface to IOnlineFilesystem

--- src/app/filesystem/online_filesystem.ts
+++ src/app/filesystem/online_filesystem.ts
@@ -2,8 +2,13 @@ import {IFilesystem} from './filesystem';
 
 export interface IOnlineFilesystem extends IFilesystem {
 
-	getUploadUrl(path: string, validUntil: Date): Promise<string>;
+	getUploadUrl(path: string, validUntil: Date, options?: GetUrlOptions): Promise<string>;
 
-	getDownloadUrl(path: string, validUntil: Date): Promise<string>;
+	getDownloadUrl(path: string, validUntil: Date, options?: GetUrlOptions): Promise<string>;
 
+}
+
+export interface GetUrlOptions {
+	contentType?: string;
+	[key: string]: any
 }
